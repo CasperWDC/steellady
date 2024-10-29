a5dd96bdc283a7d968d8263edcf57449e9c30cad
mykyt <casperwdc@gmail.com>
2024-10-29 17:47:25 +0200

    React configuration

---
+++ steellady/src/App.tsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+import Home from './pages/Home';
+
+const App = () => {
+    return (
+        <Router>
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+            </nav>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<Home />} />
+            </Routes>
+        </Router>
+    );
+};
+
+export default App;
---
+++ steellady/src/pages/Home.tsx
@@ -0,0 +1,19 @@
+import { Helmet } from 'react-helmet-async';
+
+function Home() {
+
+    return (
+        <>
+            <Helmet>
+                <title>Home Page</title>
+                <meta name="description" content="Welcome to the home page" />
+                <meta property="og:title" content="Home Page" />
+                <meta property="og:description" content="Welcome to the home page" />
+            </Helmet>
+
+            <h1>Vite + React</h1>
+        </>
+    )
+}
+
+export default Home

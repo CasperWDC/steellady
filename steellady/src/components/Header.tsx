1c3c4efca0403c36722794ec400c273a47a60817
mykyt <casperwdc@gmail.com>
2024-10-30 18:29:03 +0200

    Header

--- steellady/src/components/Header.tsx
+++ steellady/src/components/Header.tsx
@@ -1,16 +1,28 @@
 import {Link} from "react-router-dom";
 import './header.scss'
-
+import Logo from '../assets/img/logo-test.png'
 function Header() {
 
     return (
-        <>
-            <nav>
-                <Link to="/">Home</Link>
-                <Link to="/about">About</Link>
-            </nav>
-            <h1>Header</h1>
-        </>
+        <div className='header'>
+            <div className="header_container">
+                <div className="header_topRow">
+                    <img src={Logo} alt=""/>
+                    <a href='#' className="header_adress">
+                        <span>г. Донецк, пр-т Мира, 15.</span>
+                        <span>БЦ «Centaur Plaza 1», 9 этаж, офис №92</span>
+                    </a>
+                </div>
+                <div className="header_navigation">
+                    <nav>
+                        <Link to="/">Главная</Link>
+                        <Link to="/#about">О нас</Link>
+                        <Link to="/#service">Услуги</Link>
+                        <Link to="/#contacts">Контакты</Link>
+                    </nav>
+                </div>
+            </div>
+        </div>
     )
 }
 
